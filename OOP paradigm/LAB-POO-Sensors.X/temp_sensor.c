0d82e72f6f66e1091e286788bea109a28d5c595e
oscarlr16 <oscarleon.ro16@gmail.com>
2023-09-03 12:35:55 -0600

    feat(temp_sensor.c): added set_id_temp_sensor and get_id_temp_sensor static functions

--- OOP paradigm/LAB-POO-Sensors.X/temp_sensor.c
+++ OOP paradigm/LAB-POO-Sensors.X/temp_sensor.c
@@ -5,6 +5,8 @@
 static void init_temp_sensor(void* p_sensor, uint8_t *name,uint8_t id);
 static void set_name_temp_sensor(void *p_sensor, uint8_t *name);
 static uint8_t* get_temp_sensor_name(void* p_sensor);
+static void set_id_temp_sensor(void *p_sensor, uint8_t id);
+static uint8_t get_id_temp_sensor(void *p_sensor);
 
 //Funcion costructora
 temp_sensor_t* new_temp_sensor(temp_sensor_t *p_sensor){
@@ -12,6 +14,8 @@ temp_sensor_t* new_temp_sensor(temp_sensor_t *p_sensor){
     p_sensor->sensor.init = init_temp_sensor;
     p_sensor->sensor.set_name = set_name_temp_sensor;
     p_sensor->sensor.get_name = get_temp_sensor_name;
+    p_sensor->sensor.set_id = set_id_temp_sensor;
+    p_sensor->sensor.get_id = get_id_temp_sensor;
     return p_sensor;
 }
 
@@ -34,4 +38,25 @@ static void set_name_temp_sensor(void *p_sensor, uint8_t *name){
 static uint8_t* get_temp_sensor_name(void* p_sensor){
     temp_sensor_t *p_temp_sensor = (temp_sensor_t*)p_sensor;
     return p_temp_sensor->sensor.name;
+}
+
+// Function to set the ID for a temperature sensor
+static void set_id_temp_sensor(void *p_sensor, uint8_t id){
+    // Declare a pointer p_temp_sensor that can point to a struct of type temp_sensor_t
+    temp_sensor_t *p_temp_sensor;
+
+    // Cast the generic pointer p_sensor to the specific type temp_sensor_t
+    // This allows us to work with the pointer as if it were pointing to a temperature structure
+    // rather than a generic data type.
+    p_temp_sensor = (temp_sensor_t*)p_sensor;
+
+    // From here, p_temp_sensor can be used to access and modify the members
+    // of the temperature_sensor struct, if needed.
+    p_temp_sensor->sensor.id = id;
+}
+
+static uint8_t get_id_temp_sensor(void *p_sensor){
+    temp_sensor_t *p_temp_sensor;
+    p_temp_sensor = (temp_sensor_t*)p_sensor;
+    return p_temp_sensor->sensor.id;
 }
