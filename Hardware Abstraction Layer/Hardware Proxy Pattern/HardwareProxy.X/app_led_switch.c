780a3a595422171c3f6f42a646632ed378fdb4f4
oscarlr16 <oscarleon.ro16@gmail.com>
2023-10-08 17:49:42 -0600

    fix(app-main): update app and main functions for LED control
    - Add calls to new LED control functions in app_led_switch.
    - Include system initialization in main function.

--- Hardware Abstraction Layer/Hardware Proxy Pattern/HardwareProxy.X/app_led_switch.c
+++ Hardware Abstraction Layer/Hardware Proxy Pattern/HardwareProxy.X/app_led_switch.c
@@ -1,3 +1,6 @@
+#include "system.h"
+
 void app_led_switch(){
-    
+    led_on(WIFI_CONNECTION_LED);
+    led_off(ERROR_LED);
 }
--- Hardware Abstraction Layer/Hardware Proxy Pattern/HardwareProxy.X/main.c
+++ Hardware Abstraction Layer/Hardware Proxy Pattern/HardwareProxy.X/main.c
@@ -1,6 +1,8 @@
 #include "app_led_switch.h"
+#include "system.h"
 
 int main(void) {
+    system_initialize(system_state_polling);
     while(1){
         app_led_switch();
     }
